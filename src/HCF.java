1fe36f179c0f35118ef2b25dd67706ef068b2686
Amaan Ullah <theamaan13@gmail.com>
2023-08-15 00:29:23 +0530

    to find HCF of a number

---
+++ src/HCF.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class HCF {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int num1=sc.nextInt();
+        System.out.println("Enter the second number");
+        int num2=sc.nextInt();
+        int product=num1*num2;
+        int h=0;
+        for (int i = 1; i <=product; i++) {
+            if((num1%i==0)&&(num2%i==0)){
+                h=i;
+            }
+        }
+        System.out.println("The HCF is "+h);
+    }
+}
