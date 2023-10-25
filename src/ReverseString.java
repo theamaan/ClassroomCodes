cd45c5d2013fee18211ccbfbc87541dc7c8d5bac
Amaan Ullah <theamaan13@gmail.com>
2023-10-25 19:49:59 +0530

    This is a program to reverse a String using String only

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,22 +1,21 @@
 public class ReverseString {
     public static void main(String[] args) {
-        // Create a StringBuilder initialized with the string "akaza"
-        StringBuilder stringBuilder = new StringBuilder("akaza");
+        String word = "Hello this is a String";
+        String empty = "";
 
-        // Iterate over the first half of the string
-        for (int i = 0; i < stringBuilder.length() / 2; i++) {
-            int frontIndex = i; // Index of the character from the front of the string
-            int backIndex = stringBuilder.length() - 1 - i; // Index of the character from the back of the string
-
-            char frontChar = stringBuilder.charAt(frontIndex); // Character at the front index
-            char backChar = stringBuilder.charAt(backIndex); // Character at the back index
-
-            // Swap the characters
-            stringBuilder.setCharAt(frontIndex, backChar);
-            stringBuilder.setCharAt(backIndex, frontChar);
+        for (int i = word.length()-1; i >=0 ; i--) {
+            empty+=word.charAt(i);
         }
-
-        // Print the reversed string
-        System.out.println(stringBuilder);
+        System.out.println(empty);
     }
 }
+//***********************************************************
+//While this code works, it's not the most efficient way to reverse a string due to the use of string concatenation within a loop. It creates a new string in each iteration, which can be computationally expensive for longer strings.
+// Here's an optimized version using a StringBuilder:
+//public class ReverseString {
+//    public static void main(String[] args) {
+//        String word = "Hello this is a String";
+//        StringBuilder reversed = new StringBuilder(word).reverse();
+//        System.out.println(reversed);
+//    }
+//}
